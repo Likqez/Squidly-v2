bfb5f0ce40cab90209c62e2fe2b3f36ff1d64007
Lukas Lanzner <LanznerLukas@dotspace.dev>
2022-04-17 20:23:08 +0200

    removed possibility to add headers to HttpRequestFactory

--- backend/src/main/java/dev/dotspace/squidly/HttpRequestFactory.java
+++ backend/src/main/java/dev/dotspace/squidly/HttpRequestFactory.java
@@ -15,8 +15,6 @@ public class HttpRequestFactory {
   private static final HttpClient DEFAULT_CLIENT = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).connectTimeout(Duration.ofSeconds(15)).build();
   private final HttpClient httpClient;
 
-  private final List<String> headers = new ArrayList<>();
-
   private final String uri;
   private String path = "";
 
@@ -33,17 +31,6 @@ public class HttpRequestFactory {
   public HttpRequestFactory(String uri, HttpClient httpClient) {
     this.uri = uri;
     this.httpClient = httpClient;
-  }
-
-  public HttpRequestFactory header(String k, String v) {
-    this.headers.add(k);
-    this.headers.add(v);
-    return this;
-  }
-
-  public HttpRequestFactory headers(String... headers) {
-    this.headers.addAll(Arrays.asList(headers));
-    return this;
   }
 
   public HttpRequestFactory addPath(String path) {
@@ -63,7 +50,6 @@ public class HttpRequestFactory {
 
   public CompletableFuture<HttpResponse<String>> asyncGET() {
     var req = HttpRequest.newBuilder(URI.create(this.getFullUri()))
-        //.headers(this.headers.toArray(new String[0]))
         .build();
 
     return this.httpClient.sendAsync(req, HttpResponse.BodyHandlers.ofString());
