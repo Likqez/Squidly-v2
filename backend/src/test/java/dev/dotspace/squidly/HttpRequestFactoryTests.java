32624744780e00319986b77ea31b91899705ef71
Lukas Lanzner <LanznerLukas@dotspace.dev>
2022-04-20 18:30:36 +0200

    improve HttpRequestFactoryTests

--- backend/src/test/java/dev/dotspace/squidly/HttpRequestFactoryTests.java
+++ backend/src/test/java/dev/dotspace/squidly/HttpRequestFactoryTests.java
@@ -1,31 +1,30 @@
 package dev.dotspace.squidly;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HttpRequestFactoryTests {
 
-  @ParameterizedTest
+  @ParameterizedTest(name = "#{index} - {arguments}")
   @ValueSource(strings = { "https://api.paladins.com/paladinsapi.svc", "https://api.paladins.com/", "https://api.paladins.com/paladinsapi.svc/" })
-  @DisplayName("Checks corretness of URL's build from HttpRequestFactory")
-  public void testOutputURI(String uri) {
+  public void checkURICreation(String uri) {
     final var v = "1126";
     final var t = "XXXXXXX";
     final var x = "abcd";
     final var path = v + "/" + t + "/" + x;
 
-    Assertions.assertAll(() -> {
+    assertAll(() -> {
       var factory = new HttpRequestFactory(uri);
-      Assertions.assertEquals(uri, factory.getFullUri());
+      assertEquals(uri, factory.getFullUri());
 
       factory.addPath(v).addPath(t);
-      Assertions.assertEquals(uri.concat("/%s/%s".formatted(v, t)), factory.getFullUri());
+      assertEquals(uri.concat("/%s/%s".formatted(v, t)), factory.getFullUri());
 
       factory.setPath(path);
-      Assertions.assertEquals(uri.concat("/%s/%s/%s".formatted(v, t, x)), factory.getFullUri());
+      assertEquals(uri.concat("/%s/%s/%s".formatted(v, t, x)), factory.getFullUri());
     });
 
 
