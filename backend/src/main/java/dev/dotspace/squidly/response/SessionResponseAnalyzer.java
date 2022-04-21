e569aca1bcda8a777c102a39b450b57e6fca907c
Lukas Lanzner <LanznerLukas@dotspace.dev>
2022-04-21 20:13:16 +0200

    improved SessionResponseAnalyzer, now utilizing json schema validation

--- backend/src/main/java/dev/dotspace/squidly/response/SessionResponseAnalyzer.java
+++ backend/src/main/java/dev/dotspace/squidly/response/SessionResponseAnalyzer.java
@@ -1,32 +1,57 @@
 package dev.dotspace.squidly.response;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import com.qindesign.json.schema.MalformedSchemaException;
+import com.qindesign.json.schema.Validator;
+import com.qindesign.json.schema.net.URI;
 import dev.dotspace.squidly.session.SessionStore;
 
+import java.io.InputStreamReader;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+import static com.qindesign.json.schema.net.URI.parseUnchecked;
 
 public class SessionResponseAnalyzer implements JsonResponseAnalyzer {
 
+  private final Validator validator;
+
+  public SessionResponseAnalyzer() {
+    try {
+      validator = new Validator(JsonParser.parseReader(new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("schemas/create-session-schema.json")))), parseUnchecked("https://api.paladins.com/paladinsapi.svc/createsessionjson"), null, null, null);
+    } catch (MalformedSchemaException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   @Override
   public AnalysisResult<SessionStore> analyse(JsonObject jsonObject) {
-    var success = true;
+    try {
+      var success = validator.validate(jsonObject, null, null);
 
-    success = jsonObject.has("session_id");
+      if (success) {
+        var creationTime = ZonedDateTime.parse(jsonObject.get("timestamp").getAsString(), RESPONSE_TIME_FORMATTER);
+        var invalidationTime = creationTime.plus(15, ChronoUnit.MINUTES);
+        var session = jsonObject.get("session_id").getAsString();
 
-    var creationTime = jsonObject.has("timestamp") ? ZonedDateTime.parse(jsonObject.get("timestamp").getAsString(), RESPONSE_TIME_FORMATTER) : null;
+        return new AnalysisResult<>(
+            new SessionStore(session, creationTime, invalidationTime),
+            jsonObject.get("ret_msg").getAsString(),
+            true);
+      }
 
-    var invalidationTime = jsonObject.has("timestamp") ? creationTime.plus(15, ChronoUnit.MINUTES) : null;
-    var session = success ? jsonObject.get("session_id").getAsString() : "";
+      System.err.printf("Could not validate against create-session schema: %s%n", jsonObject);
 
-    if (success && (session.isEmpty() || session.isBlank())) {
-      System.err.println(jsonObject);
-      success = false;
+    } catch (MalformedSchemaException e) {
+      throw new RuntimeException(e);
     }
 
     return new AnalysisResult<>(
-        new SessionStore(session, creationTime, invalidationTime),
+        null,
         jsonObject.has("ret_msg") ? jsonObject.get("ret_msg").getAsString() : "no msg found",
-        success);
+        false);
   }
 }
