8ce03bcc1501f4d4a93f66d68c1f1ad580235baa
Lukas Lanzner <LanznerLukas@dotspace.dev>
2022-04-22 18:54:52 +0200

    fixed time error

--- backend/src/main/java/dev/dotspace/squidly/session/SessionStore.java
+++ backend/src/main/java/dev/dotspace/squidly/session/SessionStore.java
@@ -1,13 +1,15 @@
 package dev.dotspace.squidly.session;
 
 import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.chrono.ChronoZonedDateTime;
 
 public record SessionStore(String session, ZonedDateTime creation, ZonedDateTime expiry) {
 
   public boolean expired() {
-    var now = ChronoZonedDateTime.from(Instant.now());
+    var now = ZonedDateTime.from(OffsetDateTime.now(ZoneId.of("UTC")));
     return expiry.isEqual(now) || expiry.isBefore(now);
   }
 }
