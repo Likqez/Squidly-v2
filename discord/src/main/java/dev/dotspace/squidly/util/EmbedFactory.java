b55ee7866e167a5d47886c0faebe745fdcda30e2
Lukas Lanzner <LanznerLukas@dotspace.dev>
2023-04-14 20:57:44 +0200

    update Hi-Rez Copyright notice

--- discord/src/main/java/dev/dotspace/squidly/util/EmbedFactory.java
+++ discord/src/main/java/dev/dotspace/squidly/util/EmbedFactory.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 
 public interface EmbedFactory<T> {
 
-  String SUCCESS_RESPONSE_FOOTER = "Data provided by Hi-Rez. \u00a9 2022 Hi-Rez Studios, Inc. All rights reserved.";
+  String SUCCESS_RESPONSE_FOOTER = "Data provided by Hi-Rez. \u00a9 2023 Hi-Rez Studios, Inc. All rights reserved.";
   String ERROR_RESPONSE_FOOTER = "%s - Please share this ID with Squildy maintainers";
   String DEFAULT_AVATAR_URL = "https://raw.githubusercontent.com/luissilva1044894/hirez-api-docs/master/.assets/paladins/avatar/0_1.png";
   String NO_FRIENDS_GIF = "https://raw.githubusercontent.com/luissilva1044894/hirez-api-docs/master/.assets/paladins/avatar/25161.gif";
